c918d838d7e427966ba2c7f9c501fa1d11c39179
edmirh <edmir.h@gmail.com>
2020-02-07 21:41:07 +0100

    main.c updated

--- server/main.c
+++ server/main.c
@@ -6,67 +6,55 @@
 #include "control_server.h"
 #include "led.h"
 
-///----Simboli za kontrolu----///
-#define CONNECT				97
-#define FREE_CHANNEL		98
-#define CONFIRM				99
-#define CALL				100
-#define HANG_UP				101
-
-char ADDR_SRV[6] = "CHSRV"; 											// Adresa servera
-char ADDR_BUS[6] = "CHBUS";												// Adresa busa
-												
-char CALLS[5][5] = {"0x00", "0x00", "0x00", "0x00", "0x00"};	
-char USED_ADDR[5][5] = {"alpha", "charl", "bravo", "delta", "echoo"};		
-
-char ADDR_TX_P0[5] = "alpha";													
-char ADDR_TX_P1[5] = "charl";
-char ADDR_TX_P2[5] = "bravo";
-char ADDR_TX_P3[5] = "delta";
-char ADDR_TX_P4[5] = "echoo";
-
 int main(void)
 {	
-	uint8_t cnt_addr = 0;
-	uint8_t k, i, res;
-	uint8_t nrf_data[32];
-	uint8_t bus_flag = 0;
-	uint8_t z;
-	uint8_t commands[2];	
+	//uint8_t cnt_addr = 0;
+	//uint8_t k, i, res;
+	//uint8_t nrf_data[32];
+	//uint8_t bus_flag = 0;
+	//uint8_t z;
+	//uint8_t commands[2];	
 	
 	initBlink();
 	initUSART2(USART2_BAUDRATE_921600);
 	initNRF24L01(ADDR_SRV);
     startBlink("red");
             	        
-    setRxAddrNRF24L01((uint8_t *)ADDR_SRV, NRF24L01_RX_ADDR_P1);		// adresa servera
-            	        
 	while(1)															// vrti sve dok se ne unese odgovarajuća antena(0-5)
 	{
 		setTxAddrNRF24L01(ADDR_BUS);									// adresa sa koje sluša
-		res = dataReadyNRF24L01();
+		AntenaState = dataReadyNRF24L01();
 				
 		if(bus_flag == 0) {
 			bus_flag = 1;
 			
-			if(res == (NRF_DATA_READY))
+			if(AntenaState == (NRF_DATA_READY))
 			{
-				rxDataNRF24L01(nrf_data);								// primljeni podatak je upisan u nrf_data
-				commands[0] = nrf_data[0];
+				rxDataNRF24L01(RxData);								    // primljeni podatak je upisan u nrf_data
+				commands[0] = RxData[0];
 				printUSART2("%d \n", commands[0]);
 				if(commands[0] == (CONNECT)) {//connect
-					printUSART2("COMMAND A EXECUTION \n");
-					uint8_t nrf2[NRF24L01_PIPE_LENGTH];
-					for(i=0;i<32;i++) {
-						if(i < 5) 
-							nrf2[i] = (uint8_t)(USED_ADDR[cnt_addr][i]);
-						else
-							nrf2[i] = 1;
+					if(cnt_addr < 5) {
+					for(i=0;i<5;i++) {
+						appendTx((uint8_t)(USED_ADDR[cnt_addr][i]));
+					}	
+					delay_ms(1000);	  
+					txDataNRF24L01((uint8_t*)ADDR_BUS, TxData);
+					clearTx();
+					cnt_addr++;
+					commands[0] = (CALL);
 					}
-					
-					delay_ms(2000);
-					
-					txDataNRF24L01((uint8_t*)ADDR_BUS, nrf2);
+					else {
+						appendTx('e');
+						appendTx('r');
+						appendTx('r');
+						appendTx('o');
+						appendTx('r');
+						txDataNRF24L01((uint8_t*)ADDR_BUS, TxData);
+						clearTx();
+						commands[0] = (NO_MORE_ADDRESS);
+					}
+					txDataNRF24L01((uint8_t*)ADDR_BUS, TxData);
 					bus_flag = 0;
 					cnt_addr++;
 					commands[0] = (CALL);
