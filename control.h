fc05e3a213a5d634e6d8fc3620dd513f11992eef
qdnqn <qdnqns@gmail.com>
2020-02-07 14:26:14 -0800

    Added control.h

--- control.h
+++ control.h
@@ -2,9 +2,10 @@
 #define CONTROL_CLIENT_H
 
 #define BOOT			    0
-#define CHOOSE_OPTION		1
-#define WAIT_FOR_CALL		2
-#define CALL_CONTROL   		3
+#define ADDRESS				1
+#define CHOOSE_OPTION		2
+#define WAIT_FOR_CALL		3
+#define CALL_CONTROL   		4
 #define ADDR_OK				5
 #define FETCHED_OK			6
 #define GET_ADDR_OK 		7
